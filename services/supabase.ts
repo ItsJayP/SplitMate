d5fef8c0d8900eb67e235afb05d550c5dd80558b
JP <jay.patel.py@gmail.com>
2025-02-05 01:55:14 -0600

    add AsyncStorage support for Supabase client authentication

--- services/supabase.ts
+++ services/supabase.ts
@@ -2,8 +2,16 @@
 // This file is used to initialize the Supabase client, which is then used in the other files to interact with the Supabase database.
 
 import { createClient } from '@supabase/supabase-js';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const SUPABASE_URL = 'https://vbeadmbosfnaxmunkoft.supabase.co';
 const SUPABASE_ANON_KEY = 'process.env.SUPABASE_KEY';
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    storage: AsyncStorage,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
